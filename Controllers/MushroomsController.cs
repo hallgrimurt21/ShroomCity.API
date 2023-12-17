f0cdc415d142ac68d047ee802b153ae64f813563
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-17 14:42:35 +0000

    ShroomCity.API template

---
+++ Controllers/MushroomsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShroomCity.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MushroomsController : ControllerBase
+{
+    // TODO: Setup routes
+}
