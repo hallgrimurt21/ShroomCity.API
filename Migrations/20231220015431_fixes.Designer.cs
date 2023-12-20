4ffccd6c25e8e8d69759e4d05a37e4c337caeddc
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 01:55:23 +0000

    version control

--- Migrations/20231220015431_fixes.Designer.cs
+++ Migrations/20231220015431_fixes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShroomCity.Repositories.Implementations;
@@ -11,9 +12,11 @@ using ShroomCity.Repositories.Implementations;
 namespace ShroomCity.API.Migrations
 {
     [DbContext(typeof(ShroomCityDbContext))]
-    partial class ShroomCityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231220015431_fixes")]
+    partial class fixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
