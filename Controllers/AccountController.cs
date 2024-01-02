9a60352992da14983e80ead2d376184940f62427
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2024-01-02 21:12:39 +0000

    Add account controller routes

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,36 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace ShroomCity.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using ShroomCity.Models.InputModels;
 
 [ApiController]
 [Route("api/[controller]")]
 public class AccountController : ControllerBase
 {
-    // TODO: Setup routes
+    // POST api/account/register
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterInputModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    // POST api/account/login
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginInputModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/account/logout
+    [HttpGet("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/account/profile
+    [HttpGet("profile")]
+    public async Task<IActionResult> GetProfile()
+    {
+        throw new NotImplementedException();
+    }
 }
