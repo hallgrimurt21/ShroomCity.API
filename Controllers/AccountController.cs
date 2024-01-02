f428f2ef38e06c654975f33e0f29d5cffc873085
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2024-01-02 21:16:20 +0000

    Add account and token services to AccountController constructor

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,21 @@
 namespace ShroomCity.API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using ShroomCity.Models.InputModels;
+using ShroomCity.Services.Interfaces;
 
 [ApiController]
 [Route("api/[controller]")]
 public class AccountController : ControllerBase
 {
+    private readonly IAccountService accountService;
+    private readonly ITokenService tokenService;
+
+    public AccountController(IAccountService accountService, ITokenService tokenService)
+    {
+        this.accountService = accountService;
+        this.tokenService = tokenService;
+    }
+
     // POST api/account/register
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterInputModel model)
