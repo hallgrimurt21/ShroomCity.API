05d14bb862205fc2a6e3aa7b7b801d9260b105bc
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2024-01-04 11:25:20 +0000

    Add skeleton routes and authorization

--- Controllers/ResearchersController.cs
+++ Controllers/ResearchersController.cs
@@ -1,10 +1,43 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace ShroomCity.API.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShroomCity.Models.InputModels;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class ResearchersController : ControllerBase
 {
-    // TODO: Setup routes
+    // GET /api/researchers
+    [HttpGet]
+    [Authorize(Policy = "read:researchers")]
+    public async Task<IActionResult> GetResearchers()
+    {
+        throw new NotImplementedException();
+    }
+
+    // POST /api/researchers
+    [HttpPost]
+    [Authorize(Policy = "write:researchers")]
+    public async Task<IActionResult> CreateResearcher([FromBody] ResearcherInputModel researcherInputModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET /api/researchers/{id}
+    [HttpGet("{id}")]
+    [Authorize(Policy = "read:researchers")]
+    public async Task<IActionResult> GetResearcher(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET /api/researchers/self
+    [HttpGet("self")]
+    [Authorize(Policy = "write:mushrooms")]
+    public async Task<IActionResult> GetSelfResearcher()
+    {
+        throw new NotImplementedException();
+    }
 }
